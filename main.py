f149654ae8b6d16a7664e27598aaf684710df72d
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-03-07 21:40:24 +0000

    Create hike_apy

--- main.py
+++ main.py
@@ -6,17 +6,6 @@ TWILIO_SID = os.environ['TWILIO_SID']
 TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 
 def main():
-    # texter = Sms(TWILIO_SID,TWILIO_AUTH_TOKEN)
-    # texter.send_message("howdy")
-
-    # client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
-    # message = client.messages.create(
-    #     from_ = "+18337970829",
-    #     to = "+18609679255",        
-    #     body = "Testing!"
-    # )
-    # print(message.sid)
-
     messenger = Sms(TWILIO_SID,TWILIO_AUTH_TOKEN)
     messenger.send_message("Message sent Via Sms.py Class!")
     
