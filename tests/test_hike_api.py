703a86b7f94630944dbe11a2a5b45c5064696fad
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-03-11 17:28:43 +0000

    create unit test of hike_api.py

---
+++ tests/test_hike_api.py
@@ -0,0 +1,12 @@
+import pytest
+from hike_api import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Hello World!"
