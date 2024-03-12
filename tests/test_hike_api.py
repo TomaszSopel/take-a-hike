fb2782921e9da76927b7ea1474f317246b785fd5
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-03-12 02:45:22 +0000

    modify hike_api.py unit test to account for separated functions

--- tests/test_hike_api.py
+++ tests/test_hike_api.py
@@ -1,15 +1,22 @@
 import unittest
 from unittest.mock import patch
-from hike_api import hello_world
+from hike_api import hello_world, receive_text
 
-class TestSmsFunctionality(unittest.TestCase):
+class TestHelloWorld(unittest.TestCase):
+    """A class that tests hello_world()"""
 
-    @patch('hike_api.Sms')  # Mock the Sms class
-    def test_send_message_called(self, mock_sms):
-        hello_world()  # Call the endpoint function
-
-        # Assert that Sms object was created
-        mock_sms.assert_called_once() 
-
-        # Assert that send_message was called on the mock
+    @patch('hike_api.Sms')
+    def test_hello_world(self, mock_sms):
+        hello_world()
+        mock_sms.assert_called_once()
         mock_sms.return_value.send_message.assert_called_once_with("Get Message Sent")
+
+# class TestReceiveText(unittest.TestCase):
+#     """A class that tests receive_text()"""
+
+#     @patch('hike_api.request')
+#     def test_receive_text(self, mock_request):
+#         mock_request.method = "POST"
+#         mock_request.get_json().return_value = # INSERT TWILIO RESPONSE FORMAT HERE
+        
+#         receive_text()
