0f0bf49b8c763c09f0fac537440a3e37685cde76
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-03-12 16:24:25 +0000

    Testing Hike_api.py

--- hike_api.py
+++ hike_api.py
@@ -22,9 +22,9 @@ def receive_text():
     if request.method == 'POST':
         message_body = request.get_json()
         
-        messenger = Sms()
-        messenger.send_message("Message received! 1/2")
-        messenger.send_message("Message received! 2/2")
+    messenger = Sms()
+    messenger.send_message("Message received! 1/2")
+    messenger.send_message("Message received! 2/2")
 # TODO 3: Run Flask Application
 if __name__ == "__main__":
     app.run(debug=True)
