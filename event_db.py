1560f4d7eae393f294a7637593bf5ff16e0d3c32
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-04-10 22:25:43 +0000

    begin working on connecting postgreSQL database in event_db.py

---
+++ event_db.py
@@ -0,0 +1,22 @@
+import os
+import psycopg2
+
+def connect():
+    conn = psycopg2.connect(
+        host = os.getenv('POSTGRES_HOST'),
+        database = os.getenv('POSTGRES_DB'),
+        user = os.getenv('POSTGRES_USER'),
+        password = os.getenv('POSTGRES_PASSWORD')
+    )
+    return conn
+
+connection = connect()
+cur = connection.cursor()
+
+cur.execute("SELECT version();")
+result = cur.fetchone()
+
+print(result)
+
+cur.close()
+conn.close()
