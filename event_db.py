d1bd23c8e735325ed89b8c5df5a681556ea5fc20
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-08-12 21:12:11 +0000

    connect event_db.py to database

--- event_db.py
+++ event_db.py
@@ -1,23 +1,35 @@
 import os
 import psycopg2
 
+HOST = 'localhost'
+PORT = '5432'
+DATABASE = "take_a_hike"
+USER = "gitpod"
+
+
+
 def connect():
     conn = psycopg2.connect(
-        host = "localhost",
-        port = "5432",
-        database = "take_a_hike",
-        password = "7890",
-        user = "postgres"
+        host = HOST,
+        port = PORT,
+        database = DATABASE,
+        # password = "7890",
+        user = USER
     )
     return conn
 
 connection = connect()
 cur = connection.cursor()
 
-cur.execute("SELECT version();")
-result = cur.fetchone()
+# cur.execute("SELECT * FROM users;")
+# result = cur.fetchall()
+# print(result)
 
-print(result)
+def get_user(number:str):
+    id = cur.execute("SELECT user_id FROM users WHERE phone_number = f'{number}'")
+    print(id)
+
+get_user('8609673158')
 
 cur.close()
 connection.close()
