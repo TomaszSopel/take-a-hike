6af461ad5c5a9d6c0f8810ad53ba3a3b67f50816
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-08-13 19:05:42 +0000

    define functionality for retreiving single user id or phone number

--- event_db.py
+++ event_db.py
@@ -21,15 +21,25 @@ def connect():
 connection = connect()
 cur = connection.cursor()
 
-cur.execute("SELECT * FROM users;")
-result = cur.fetchall()
-print(result)
+# cur.execute("SELECT * FROM users;")
+# result = cur.fetchone()
+# print(result)
 
-# def get_user(number:str):
-#     id = cur.execute("SELECT user_id FROM users WHERE phone_number = f'{number}'")
-#     print(id)
+def get_user(number:str):
+    """Inputs a single phone number (str) and returns a single user_id (int)"""
+    cur.execute(f"SELECT user_id FROM users WHERE phone_number = '{number}'")
+    user_id = cur.fetchone()
+    return user_id[0]
 
-# get_user('8609673158')
+print(get_user("1234567890"))
 
-cur.close()
-connection.close()
+def get_number(id:int):
+    """Inputs a user id (int) and returns their corresponding phone number (str)"""
+    cur.execute(f"SELECT phone_number FROM users WHERE user_id = '{id}'")
+    id = cur.fetchone()
+    return id[0]
+
+print(get_number(1))
+
+# cur.close()
+# connection.close()
