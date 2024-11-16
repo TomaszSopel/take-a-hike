5c2f6113d148ca0a6cac4fa65c7fff8ad1908529
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-11-16 21:32:02 +0000

    remove unnecessary code in main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import os
-from sms import Sms
-from twilio.rest import Client
 from hike_api import app
 import logging
 
 def main():
     logging.info("Main Started")
-    
 
 if __name__ == "__main__":
     main()
