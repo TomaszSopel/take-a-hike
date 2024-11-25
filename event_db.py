0c2878b802912b363e2fbef8ede8a12af4067386
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-11-25 22:32:23 +0000

    Revert "Reapply "modify all functions to incorporate discreet open and closing of connections""
    This reverts commit bfea2592d3949ece20ee577c2ec787f4555dcf00.

--- event_db.py
+++ event_db.py
@@ -29,41 +29,30 @@ def get_user(number:str):
     """Inputs a single phone number (str) and returns a single user_id (int)
     if they are in the users table, otherwise returns FALSE"""
     try:
-        connection = open_connection()
-        cur = connection.cursor()
         cur.execute(f"SELECT user_id FROM users WHERE phone_number = '{number}'")
         user_id = cur.fetchone()
         return user_id[0]
     except TypeError:
         return False
     finally:
-        close_connection(connection, cur)
+        close()
 
 def get_phone(id:int):
     """Inputs a user id (int) and returns their corresponding phone number (str). If not found, returns False."""
     try:
-        connection = open_connection()
-        cur = connection.cursor()
         cur.execute(f"SELECT phone_number FROM users WHERE user_id = '{id}'")
         id = cur.fetchone()
         return id[0]
     except TypeError:
         return False
-    finally:
-        close_connection(connection, cur)
 
 def log_user(number:str):
     """Inputs a phone number, if it already doesn't exist in the users table,
     it will add it."""
     if get_user(number) is False:
-        try:
-            connection = open_connection()
-            cur = connection.cursor()
-            cur.execute(f"INSERT INTO users (phone_number) VALUES ({number});")
-            connection.commit()
-            print(f"New user added, phone number: {number}")
-        finally:
-            close_connection(connection, cur)
+        cur.execute(f"INSERT INTO users (phone_number) VALUES ({number});")
+        connection.commit()
+        print(f"New user added, phone number: {number}")
     else:
         pass
 
@@ -71,49 +60,34 @@ def get_event_id(code:str):
     """Inputs an event event code (str) and checks if the corresponding event 
     exists in the events table, returning event id (int), otherwise False"""
     try:
-        connection = open_connection()
-        cur = connection.cursor()
         cur.execute(f"SELECT event_id FROM events WHERE event_code = '{code.lower()}'")
         event_id = cur.fetchone()
         return event_id[0]
     except TypeError:
         return False
-    finally:
-        close_connection(connection, cur)
 
 # TODO 1: Modify sign_up() to refuse duplicate signups
 def sign_up(user_id:int, event_id:int):
     """Inputs user id and event id and logs them into the user_events_signup table."""
     try:
-        connection = open_connection()
-        cur = connection.cursor()
         cur.execute(f"INSERT INTO user_event_signups (user_id, event_id) VALUES ({user_id},{event_id});")
         connection.commit()
     except psycopg2.errors.ForeignKeyViolation:
         print("File not Found: You can control this error!!!")
-    finally:
-        close_connection(connection, cur)
 
 def event_get_numbers(event_id:int):
     """Inputs an event code (int) and returns all phone numbers signed up for the event (list[int])"""
     try:
-        connection = open_connection()
-        cur = connection.cursor()
         cur.execute(f"SELECT user_id FROM user_event_signups WHERE event_id = {event_id};")
         return (cur.fetchall())
     except:
         print("Error")
-    finally:
-        close_connection(connection, cur)
 
 def get_events():
     """Returns all events currently registered"""
     try:
-        connection = open_connection()
-        cur = connection.cursor()
         cur.execute(f"SELECT event_code FROM events;")
         return (cur.fetchall())
     except:
         print("Error")
-    finally:
-        close_connection(connection, cur)
+
