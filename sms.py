e48e22539bbfb3c8ba14ccd18be2f74428e5ccb8
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2024-11-27 21:42:11 +0000

    add function that allows users to sign up for events via SMS

--- sms.py
+++ sms.py
@@ -8,9 +8,9 @@ class Sms:
         self.account_sid = os.environ.get('TWILIO_SID')
         self.auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
         self.client = Client(self.account_sid, self.auth_token)
-
+# TODO: Modify the send message function to require a phone number as an argument (otherwise all followup texts following a signup will be sent to the twilio number)
     def send_message(self, msg):
-        message = self.client.messages.create(
+        self.client.messages.create(
             from_=os.environ.get('TWILIO_NUMBER'),
             body=msg,
             to=os.environ.get('TESTING_NUMBER'),
--- sms_database_bridge.py
+++ sms_database_bridge.py
@@ -1,4 +1,4 @@
-import event_db
+import event_db, sms
 class Text_message_input:
     """Creates Text_message_input object that can send messages via Twilio API"""
     def __init__(self, phone_number, body:str):
@@ -9,7 +9,15 @@ class Text_message_input:
         if "signup" in self.body_list:
             events_list = event_db.get_events()
             if self.body_list[1] in events_list:
-                print("I'm Triggered!")
+                event_db.log_user(self.phone_number)
+                user_id = event_db.get_user(self.phone_number)
+                event_id = event_db.get_event_id(self.body_list[1])
+                # TODO: write a function that checks if the user is already signed up for an event
+                event_db.sign_up(user_id=user_id, event_id=event_id)
+                # TEMP CODE TO CONFIRM sign-up (these texts can ONLY be sent to the twilio number atm.)
+                messenger = sms.Sms()
+                messenger.send_message(f"Signup for {self.body_list[1].capitalize()} confirmed!")
+
             
 """Signing up:Text Cherry to 860-XXX-XXXX -->
 Checks to see if your phone number is a part of the users table, if not, it will add it -->
