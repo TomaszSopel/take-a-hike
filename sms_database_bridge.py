7796b3f5d88e0f3b4a1b168c5cdd077157c56022
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2025-01-10 22:22:27 +0000

    add test code for local testing

--- sms_database_bridge.py
+++ sms_database_bridge.py
@@ -26,9 +26,19 @@ class Text_message_input:
                 elif result is True:
                     messenger.send_message(
                         f"Signup for {self.body_list[1].capitalize()} confirmed!"
-                        ) 
+                        )
+        elif "add" in self.body_list:
+            if "admin" in self.body_list:
+                print("trigger me timbers!")
 """Signing up:Text Cherry to 860-XXX-XXXX -->
 Checks to see if your phone number is a part of the users table, 
 if not, it will add it -->
 Retreives the userID corresponding with the phone number
-signs up the phone number for the particular event"""
+signs up the phone number for the particular event
+
+Test curl command: curl -X POST http://127.0.0.1:5000/ -H "Content-Type: application/x-www-form-urlencoded" -d "Body=Signup Cherry" -d "From=18609673158"
+
+
+"""
+
+
