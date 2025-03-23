d392015f61ff4231349d9bf8ee5fa4104440571d
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2025-03-23 23:51:03 +0000

    implemented command pattern for sms.py

--- sms.py
+++ sms.py
@@ -16,12 +16,9 @@ class Sms:
             logging.error("Twilio credentials not set")
         
 # TODO: Modify the send message function to require a phone number as an argument (otherwise all followup texts following a signup will be sent to the twilio number)
-    def send_sms(self, to_number, body):
+    def send_sms(self, body, to_number=os.environ.get('TESTING_NUMBER')):
         #TODO: The send_message function can only send messages to the hard coded phone number, add a parameter in the initialization of the Sms object to include the senders phone number such that they can get the response sent to their number. 
-        
-        if not to_number:
-            to_number = os.environ.get('TESTING_NUMBER')
-        
+
         self.client.messages.create(
             from_=os.environ.get('TWILIO_NUMBER'),
             body=body,
