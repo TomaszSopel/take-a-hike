d6f5e6494270e186e717e8898bcc4a3145790ffc
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2025-06-23 19:34:07 +0000

    refactor set_admin_status to use parameterized queries and improve error handling

--- admin.py
+++ admin.py
@@ -33,14 +33,27 @@ def check_admin(phone_number:str) -> bool | None:
     finally:
         event_db.close_connection(connection, cur)
 
-def create_admin(phone_number:str):
-    """Changes the is_admin value for the user provided from False to True. If no such user exists, a ValueError"""
-    connection = event_db.open_connection()
-    cur = connection.cursor()
+def set_admin_status(phone_number:str, status:bool) -> bool:
+    """Changes the is_admin value for the user """
+    connection, cur = None, None
+    
+    normalized_phone_number = event_db.normalize_phone_number(phone_number)
+    if not normalized_phone_number:
+        return False
+    
+    query = "UPDATE users SET is_admin = %s WHERE phone_number = %s"
+    
     try:
-        cur.execute(f"UPDATE users SET is_admin = TRUE WHERE phone_number = '{phone_number}';")
-        return (f"{phone_number} now has admin priviledges.")
-    except TypeError:
-        return None
+        connection = event_db.open_connection()
+        cur = connection.cursor()
+        cur.execute(query, (status, normalized_phone_number))
+        connection.commit()
+        return cur.rowcount == 1
+    except psycopg2.Error as e:
+        logging.error(f"DB Error in set_admin_status: {e}")
+        return False
+    finally:
+        event_db.close_connection(connection, cur)
+
 
 # def create_event():
