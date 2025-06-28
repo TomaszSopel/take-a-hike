b8e04314bfbcc5af113e05973240c6608fa0bcc4
Tomasz Sopel <Tomasz.j.sopel@gmail.com>
2025-06-28 01:43:35 +0000

    implement add_event function into admin.py

--- admin.py
+++ admin.py
@@ -2,8 +2,9 @@ import event_db
 import psycopg2
 import event_db
 import logging
+import datetime
 
-""""This will handle all the admin related functions"""
+""""This file handles all the admin related functions"""
 
 def check_admin(phone_number:str) -> bool | None:
     """Takes a phone number in the form of a string and returns the specific users True or False value for is_admin.
@@ -75,6 +76,38 @@ def get_headcount(event_code:str) -> int | None:
         return count
     except psycopg2.Error as e:
         logging.error(f"DB Error in get_headcount: {e}")
+        return None
+    finally:
+        event_db.close_connection(connection, cur)
+
+def add_event(code: str, date: str, name: str) -> int | None:
+    """
+    Admin Function: Inputs the event code, date, and name of event to create a new event in the events table of the database.
+    
+    Returns new event_ID if the event is successfully created or None in the event of failure.
+    """
+
+    connection, cur = None, None
+
+    sql = "INSERT INTO events (event_code, event_name, event_date)" \
+    "VALUES (%s, %s, %s) RETURNING event_id;"
+
+    try:
+        connection = event_db.open_connection()
+        cur = connection.cursor()
+
+        event_date = datetime.date.fromisoformat(date) #Converts the date str to a date object
+
+        cur.execute(sql, (code.lower(), name, event_date))
+
+        new_id = cur.fetchone()[0]
+
+        connection.commit()
+        return new_id
+    except (psycopg2.Error, ValueError) as e:
+        logging.error(f"DB Error or invalid date format in add_event: {e}")
+        if connection:
+            connection.rollback()
         return None
     finally:
         event_db.close_connection(connection, cur)
