c9fd958e7f438504ca8d2fff52f434ad825abd34
Tomasz Sopel <tomasz.j.sopel@gmail.com>
2025-07-24 13:55:12 -0400

    Improve data integreity with schema constraints
    - Add ON DELETE CASCADE to delete user event signups when an event is deleted by an admin.
    - Adds a UNIQUE constraint to events.event_code to prevent duplicate codes, forcing admins to delete previous events if they're running a duplicate event later on.
    - Adds a UNIQUE constraint to signups.user_id and signups.event_id in order to add another safeguard for preventing duplicate entries.

--- schema.sql
+++ schema.sql
@@ -2,12 +2,13 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.20 (Ubuntu 12.20-1.pgdg22.04+1)
--- Dumped by pg_dump version 12.20 (Ubuntu 12.20-1.pgdg22.04+1)
+-- Dumped from database version 16.8
+-- Dumped by pg_dump version 17.5 (Ubuntu 17.5-0ubuntu0.25.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -16,12 +17,288 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: _heroku; Type: SCHEMA; Schema: -; Owner: heroku_admin
+--
+
+CREATE SCHEMA _heroku;
+
+
+ALTER SCHEMA _heroku OWNER TO heroku_admin;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: u459cifb94av5h
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+ALTER SCHEMA public OWNER TO u459cifb94av5h;
+
+--
+-- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statistics of all SQL statements executed';
+
+
+--
+-- Name: create_ext(); Type: FUNCTION; Schema: _heroku; Owner: heroku_admin
+--
+
+CREATE FUNCTION _heroku.create_ext() RETURNS event_trigger
+    LANGUAGE plpgsql SECURITY DEFINER
+    AS $$
+
+DECLARE
+
+  schemaname TEXT;
+  databaseowner TEXT;
+
+  r RECORD;
+
+BEGIN
+
+  IF tg_tag = 'CREATE EXTENSION' and current_user != 'rds_superuser' THEN
+    FOR r IN SELECT * FROM pg_event_trigger_ddl_commands()
+    LOOP
+        CONTINUE WHEN r.command_tag != 'CREATE EXTENSION' OR r.object_type != 'extension';
+
+        schemaname = (
+            SELECT n.nspname
+            FROM pg_catalog.pg_extension AS e
+            INNER JOIN pg_catalog.pg_namespace AS n
+            ON e.extnamespace = n.oid
+            WHERE e.oid = r.objid
+        );
+
+        databaseowner = (
+            SELECT pg_catalog.pg_get_userbyid(d.datdba)
+            FROM pg_catalog.pg_database d
+            WHERE d.datname = current_database()
+        );
+        --RAISE NOTICE 'Record for event trigger %, objid: %,tag: %, current_user: %, schema: %, database_owenr: %', r.object_identity, r.objid, tg_tag, current_user, schemaname, databaseowner;
+        IF r.object_identity = 'address_standardizer_data_us' THEN
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT, UPDATE, INSERT, DELETE', databaseowner, 'us_gaz');
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT, UPDATE, INSERT, DELETE', databaseowner, 'us_lex');
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT, UPDATE, INSERT, DELETE', databaseowner, 'us_rules');
+        ELSIF r.object_identity = 'amcheck' THEN
+            EXECUTE format('GRANT EXECUTE ON FUNCTION %I.bt_index_check TO %I;', schemaname, databaseowner);
+            EXECUTE format('GRANT EXECUTE ON FUNCTION %I.bt_index_parent_check TO %I;', schemaname, databaseowner);
+        ELSIF r.object_identity = 'dict_int' THEN
+            EXECUTE format('ALTER TEXT SEARCH DICTIONARY %I.intdict OWNER TO %I;', schemaname, databaseowner);
+        ELSIF r.object_identity = 'pg_partman' THEN
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT, UPDATE, INSERT, DELETE', databaseowner, 'part_config');
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT, UPDATE, INSERT, DELETE', databaseowner, 'part_config_sub');
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT, UPDATE, INSERT, DELETE', databaseowner, 'custom_time_partitions');
+        ELSIF r.object_identity = 'pg_stat_statements' THEN
+            EXECUTE format('GRANT EXECUTE ON FUNCTION %I.pg_stat_statements_reset TO %I;', schemaname, databaseowner);
+        ELSIF r.object_identity = 'postgis' THEN
+            PERFORM _heroku.postgis_after_create();
+        ELSIF r.object_identity = 'postgis_raster' THEN
+            PERFORM _heroku.postgis_after_create();
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT', databaseowner, 'raster_columns');
+            PERFORM _heroku.grant_table_if_exists(schemaname, 'SELECT', databaseowner, 'raster_overviews');
+        ELSIF r.object_identity = 'postgis_topology' THEN
+            PERFORM _heroku.postgis_after_create();
+            EXECUTE format('GRANT USAGE ON SCHEMA topology TO %I;', databaseowner);
+            EXECUTE format('GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO %I;', databaseowner);
+            PERFORM _heroku.grant_table_if_exists('topology', 'SELECT, UPDATE, INSERT, DELETE', databaseowner);
+            EXECUTE format('GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA topology TO %I;', databaseowner);
+        ELSIF r.object_identity = 'postgis_tiger_geocoder' THEN
+            PERFORM _heroku.postgis_after_create();
+            EXECUTE format('GRANT USAGE ON SCHEMA tiger TO %I;', databaseowner);
+            EXECUTE format('GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA tiger TO %I;', databaseowner);
+            PERFORM _heroku.grant_table_if_exists('tiger', 'SELECT, UPDATE, INSERT, DELETE', databaseowner);
+
+            EXECUTE format('GRANT USAGE ON SCHEMA tiger_data TO %I;', databaseowner);
+            EXECUTE format('GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA tiger_data TO %I;', databaseowner);
+            PERFORM _heroku.grant_table_if_exists('tiger_data', 'SELECT, UPDATE, INSERT, DELETE', databaseowner);
+        END IF;
+    END LOOP;
+  END IF;
+END;
+$$;
+
+
+ALTER FUNCTION _heroku.create_ext() OWNER TO heroku_admin;
+
+--
+-- Name: drop_ext(); Type: FUNCTION; Schema: _heroku; Owner: heroku_admin
+--
+
+CREATE FUNCTION _heroku.drop_ext() RETURNS event_trigger
+    LANGUAGE plpgsql SECURITY DEFINER
+    AS $$
+
+DECLARE
+
+  schemaname TEXT;
+  databaseowner TEXT;
+
+  r RECORD;
+
+BEGIN
+
+  IF tg_tag = 'DROP EXTENSION' and current_user != 'rds_superuser' THEN
+    FOR r IN SELECT * FROM pg_event_trigger_dropped_objects()
+    LOOP
+      CONTINUE WHEN r.object_type != 'extension';
+
+      databaseowner = (
+            SELECT pg_catalog.pg_get_userbyid(d.datdba)
+            FROM pg_catalog.pg_database d
+            WHERE d.datname = current_database()
+      );
+
+      --RAISE NOTICE 'Record for event trigger %, objid: %,tag: %, current_user: %, database_owner: %, schemaname: %', r.object_identity, r.objid, tg_tag, current_user, databaseowner, r.schema_name;
+
+      IF r.object_identity = 'postgis_topology' THEN
+          EXECUTE format('DROP SCHEMA IF EXISTS topology');
+      END IF;
+    END LOOP;
+
+  END IF;
+END;
+$$;
+
+
+ALTER FUNCTION _heroku.drop_ext() OWNER TO heroku_admin;
+
+--
+-- Name: extension_before_drop(); Type: FUNCTION; Schema: _heroku; Owner: heroku_admin
+--
+
+CREATE FUNCTION _heroku.extension_before_drop() RETURNS event_trigger
+    LANGUAGE plpgsql SECURITY DEFINER
+    AS $$
+
+DECLARE
+
+  query TEXT;
+
+BEGIN
+  query = (SELECT current_query());
+
+  -- RAISE NOTICE 'executing extension_before_drop: tg_event: %, tg_tag: %, current_user: %, session_user: %, query: %', tg_event, tg_tag, current_user, session_user, query;
+  IF tg_tag = 'DROP EXTENSION' and not pg_has_role(session_user, 'rds_superuser', 'MEMBER') THEN
+    -- DROP EXTENSION [ IF EXISTS ] name [, ...] [ CASCADE | RESTRICT ]
+    IF (regexp_match(query, 'DROP\s+EXTENSION\s+(IF\s+EXISTS)?.*(plpgsql)', 'i') IS NOT NULL) THEN
+      RAISE EXCEPTION 'The plpgsql extension is required for database management and cannot be dropped.';
+    END IF;
+  END IF;
+END;
+$$;
+
+
+ALTER FUNCTION _heroku.extension_before_drop() OWNER TO heroku_admin;
+
+--
+-- Name: grant_table_if_exists(text, text, text, text); Type: FUNCTION; Schema: _heroku; Owner: heroku_admin
+--
+
+CREATE FUNCTION _heroku.grant_table_if_exists(alias_schemaname text, grants text, databaseowner text, alias_tablename text DEFAULT NULL::text) RETURNS void
+    LANGUAGE plpgsql SECURITY DEFINER
+    AS $$
+
+BEGIN
+
+  IF alias_tablename IS NULL THEN
+    EXECUTE format('GRANT %s ON ALL TABLES IN SCHEMA %I TO %I;', grants, alias_schemaname, databaseowner);
+  ELSE
+    IF EXISTS (SELECT 1 FROM pg_tables WHERE pg_tables.schemaname = alias_schemaname AND pg_tables.tablename = alias_tablename) THEN
+      EXECUTE format('GRANT %s ON TABLE %I.%I TO %I;', grants, alias_schemaname, alias_tablename, databaseowner);
+    END IF;
+  END IF;
+END;
+$$;
+
+
+ALTER FUNCTION _heroku.grant_table_if_exists(alias_schemaname text, grants text, databaseowner text, alias_tablename text) OWNER TO heroku_admin;
+
+--
+-- Name: postgis_after_create(); Type: FUNCTION; Schema: _heroku; Owner: heroku_admin
+--
+
+CREATE FUNCTION _heroku.postgis_after_create() RETURNS void
+    LANGUAGE plpgsql SECURITY DEFINER
+    AS $$
+DECLARE
+    schemaname TEXT;
+    databaseowner TEXT;
+BEGIN
+    schemaname = (
+        SELECT n.nspname
+        FROM pg_catalog.pg_extension AS e
+        INNER JOIN pg_catalog.pg_namespace AS n ON e.extnamespace = n.oid
+        WHERE e.extname = 'postgis'
+    );
+    databaseowner = (
+        SELECT pg_catalog.pg_get_userbyid(d.datdba)
+        FROM pg_catalog.pg_database d
+        WHERE d.datname = current_database()
+    );
+
+    EXECUTE format('GRANT EXECUTE ON FUNCTION %I.st_tileenvelope TO %I;', schemaname, databaseowner);
+    EXECUTE format('GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE %I.spatial_ref_sys TO %I;', schemaname, databaseowner);
+END;
+$$;
+
+
+ALTER FUNCTION _heroku.postgis_after_create() OWNER TO heroku_admin;
+
+--
+-- Name: validate_extension(); Type: FUNCTION; Schema: _heroku; Owner: heroku_admin
+--
+
+CREATE FUNCTION _heroku.validate_extension() RETURNS event_trigger
+    LANGUAGE plpgsql SECURITY DEFINER
+    AS $$
+
+DECLARE
+
+  schemaname TEXT;
+  r RECORD;
+
+BEGIN
+
+  IF tg_tag = 'CREATE EXTENSION' and current_user != 'rds_superuser' THEN
+    FOR r IN SELECT * FROM pg_event_trigger_ddl_commands()
+    LOOP
+      CONTINUE WHEN r.command_tag != 'CREATE EXTENSION' OR r.object_type != 'extension';
+
+      schemaname = (
+        SELECT n.nspname
+        FROM pg_catalog.pg_extension AS e
+        INNER JOIN pg_catalog.pg_namespace AS n
+        ON e.extnamespace = n.oid
+        WHERE e.oid = r.objid
+      );
+
+      IF schemaname = '_heroku' THEN
+        RAISE EXCEPTION 'Creating extensions in the _heroku schema is not allowed';
+      END IF;
+    END LOOP;
+  END IF;
+END;
+$$;
+
+
+ALTER FUNCTION _heroku.validate_extension() OWNER TO heroku_admin;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: events; Type: TABLE; Schema: public; Owner: gitpod
+-- Name: events; Type: TABLE; Schema: public; Owner: u459cifb94av5h
 --
 
 CREATE TABLE public.events (
@@ -34,10 +311,10 @@ CREATE TABLE public.events (
 );
 
 
-ALTER TABLE public.events OWNER TO gitpod;
+ALTER TABLE public.events OWNER TO u459cifb94av5h;
 
 --
--- Name: events_event_id_seq; Type: SEQUENCE; Schema: public; Owner: gitpod
+-- Name: events_event_id_seq; Type: SEQUENCE; Schema: public; Owner: u459cifb94av5h
 --
 
 CREATE SEQUENCE public.events_event_id_seq
@@ -49,17 +326,17 @@ CREATE SEQUENCE public.events_event_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.events_event_id_seq OWNER TO gitpod;
+ALTER SEQUENCE public.events_event_id_seq OWNER TO u459cifb94av5h;
 
 --
--- Name: events_event_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gitpod
+-- Name: events_event_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER SEQUENCE public.events_event_id_seq OWNED BY public.events.event_id;
 
 
 --
--- Name: user_event_signups; Type: TABLE; Schema: public; Owner: gitpod
+-- Name: user_event_signups; Type: TABLE; Schema: public; Owner: u459cifb94av5h
 --
 
 CREATE TABLE public.user_event_signups (
@@ -71,10 +348,10 @@ CREATE TABLE public.user_event_signups (
 );
 
 
-ALTER TABLE public.user_event_signups OWNER TO gitpod;
+ALTER TABLE public.user_event_signups OWNER TO u459cifb94av5h;
 
 --
--- Name: user_event_signups_user_event_id_seq; Type: SEQUENCE; Schema: public; Owner: gitpod
+-- Name: user_event_signups_user_event_id_seq; Type: SEQUENCE; Schema: public; Owner: u459cifb94av5h
 --
 
 CREATE SEQUENCE public.user_event_signups_user_event_id_seq
@@ -86,17 +363,17 @@ CREATE SEQUENCE public.user_event_signups_user_event_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.user_event_signups_user_event_id_seq OWNER TO gitpod;
+ALTER SEQUENCE public.user_event_signups_user_event_id_seq OWNER TO u459cifb94av5h;
 
 --
--- Name: user_event_signups_user_event_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gitpod
+-- Name: user_event_signups_user_event_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER SEQUENCE public.user_event_signups_user_event_id_seq OWNED BY public.user_event_signups.user_event_id;
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: gitpod
+-- Name: users; Type: TABLE; Schema: public; Owner: u459cifb94av5h
 --
 
 CREATE TABLE public.users (
@@ -106,10 +383,10 @@ CREATE TABLE public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO gitpod;
+ALTER TABLE public.users OWNER TO u459cifb94av5h;
 
 --
--- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: gitpod
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: u459cifb94av5h
 --
 
 CREATE SEQUENCE public.users_user_id_seq
@@ -121,38 +398,38 @@ CREATE SEQUENCE public.users_user_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.users_user_id_seq OWNER TO gitpod;
+ALTER SEQUENCE public.users_user_id_seq OWNER TO u459cifb94av5h;
 
 --
--- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gitpod
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
 
 
 --
--- Name: events event_id; Type: DEFAULT; Schema: public; Owner: gitpod
+-- Name: events event_id; Type: DEFAULT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.events ALTER COLUMN event_id SET DEFAULT nextval('public.events_event_id_seq'::regclass);
 
 
 --
--- Name: user_event_signups user_event_id; Type: DEFAULT; Schema: public; Owner: gitpod
+-- Name: user_event_signups user_event_id; Type: DEFAULT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.user_event_signups ALTER COLUMN user_event_id SET DEFAULT nextval('public.user_event_signups_user_event_id_seq'::regclass);
 
 
 --
--- Name: users user_id; Type: DEFAULT; Schema: public; Owner: gitpod
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
 
 
 --
--- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: gitpod
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.events
@@ -160,7 +437,31 @@ ALTER TABLE ONLY public.events
 
 
 --
--- Name: user_event_signups user_event_signups_pkey; Type: CONSTRAINT; Schema: public; Owner: gitpod
+-- Name: events unique_event_code; Type: CONSTRAINT; Schema: public; Owner: u459cifb94av5h
+--
+
+ALTER TABLE ONLY public.events
+    ADD CONSTRAINT unique_event_code UNIQUE (event_code);
+
+
+--
+-- Name: users unique_phone_number; Type: CONSTRAINT; Schema: public; Owner: u459cifb94av5h
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_phone_number UNIQUE (phone_number);
+
+
+--
+-- Name: user_event_signups unique_user_event_signup; Type: CONSTRAINT; Schema: public; Owner: u459cifb94av5h
+--
+
+ALTER TABLE ONLY public.user_event_signups
+    ADD CONSTRAINT unique_user_event_signup UNIQUE (user_id, event_id);
+
+
+--
+-- Name: user_event_signups user_event_signups_pkey; Type: CONSTRAINT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.user_event_signups
@@ -168,7 +469,7 @@ ALTER TABLE ONLY public.user_event_signups
 
 
 --
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: gitpod
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.users
@@ -176,20 +477,74 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: user_event_signups user_event_signups_event_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: gitpod
+-- Name: user_event_signups user_event_signups_event_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.user_event_signups
-    ADD CONSTRAINT user_event_signups_event_id_fkey FOREIGN KEY (event_id) REFERENCES public.events(event_id);
+    ADD CONSTRAINT user_event_signups_event_id_fkey FOREIGN KEY (event_id) REFERENCES public.events(event_id) ON DELETE CASCADE;
 
 
 --
--- Name: user_event_signups user_event_signups_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: gitpod
+-- Name: user_event_signups user_event_signups_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: u459cifb94av5h
 --
 
 ALTER TABLE ONLY public.user_event_signups
     ADD CONSTRAINT user_event_signups_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id);
 
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: u459cifb94av5h
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+
+
+--
+-- Name: FUNCTION pg_stat_statements_reset(userid oid, dbid oid, queryid bigint); Type: ACL; Schema: public; Owner: rdsadmin
+--
+
+GRANT ALL ON FUNCTION public.pg_stat_statements_reset(userid oid, dbid oid, queryid bigint) TO u459cifb94av5h;
+
+
+--
+-- Name: extension_before_drop; Type: EVENT TRIGGER; Schema: -; Owner: heroku_admin
+--
+
+CREATE EVENT TRIGGER extension_before_drop ON ddl_command_start
+   EXECUTE FUNCTION _heroku.extension_before_drop();
+
+
+ALTER EVENT TRIGGER extension_before_drop OWNER TO heroku_admin;
+
+--
+-- Name: log_create_ext; Type: EVENT TRIGGER; Schema: -; Owner: heroku_admin
+--
+
+CREATE EVENT TRIGGER log_create_ext ON ddl_command_end
+   EXECUTE FUNCTION _heroku.create_ext();
+
+
+ALTER EVENT TRIGGER log_create_ext OWNER TO heroku_admin;
+
+--
+-- Name: log_drop_ext; Type: EVENT TRIGGER; Schema: -; Owner: heroku_admin
+--
+
+CREATE EVENT TRIGGER log_drop_ext ON sql_drop
+   EXECUTE FUNCTION _heroku.drop_ext();
+
+
+ALTER EVENT TRIGGER log_drop_ext OWNER TO heroku_admin;
+
+--
+-- Name: validate_extension; Type: EVENT TRIGGER; Schema: -; Owner: heroku_admin
+--
+
+CREATE EVENT TRIGGER validate_extension ON ddl_command_end
+   EXECUTE FUNCTION _heroku.validate_extension();
+
+
+ALTER EVENT TRIGGER validate_extension OWNER TO heroku_admin;
 
 --
 -- PostgreSQL database dump complete
